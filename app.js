99a00424597145b44d0da9829d74101168412df4
Anri Gogiberidze <anri45gogiberidze@gmail.com>
2022-02-08 21:33:21 +0400

    To-Do List done (one bug remains)

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
-const removeBtn = document.querySelectorAll('.removeBtn');
+// Add a new task from input
+const addTaskBtn = document.getElementById('addTaskBtn');
+const activeTask = document.querySelector('.activeTask');
+const unorderedList = document.getElementById('mainTasks');
 
-for(let i = 0; i < removeBtn.length; i++) {
-    removeBtn[i].onclick = function() {
-        const parentDiv = this.parentElement;
-        const parentLi = parentDiv.parentElement;
-        parentLi.style.display = 'none';
+addTaskBtn.addEventListener('click', () => {
+    const addTaskInp = document.getElementById('addTaskInp');
+    const taskInputVal = addTaskInp.value; //New task!!!
+
+    let newListItem = document.createElement('li');
+
+    let newTextEllDiv = document.createElement('div');
+    newTextEllDiv.classList.add('activeTask');
+
+    let newTextEllP = document.createElement('p');
+    newTextEllP.innerText = taskInputVal;
+    newTextEllP.classList.add('taskDesc');
+    newTextEllP.setAttribute('onclick', 'crossOutP()')
+    newTextEllDiv.append(newTextEllP);
+
+    let newTextEllSpan = document.createElement('span');
+    newTextEllSpan.innerHTML = '&times;'
+    newTextEllSpan.classList.add('removeBtn');
+    newTextEllSpan.setAttribute('onclick', 'removeTask()');
+    newTextEllDiv.append(newTextEllSpan);
+
+    newListItem.append(newTextEllDiv);
+    unorderedList.append(newListItem);
+})
+
+// Delete an existing task
+function removeTask() {
+    const removeBtn = document.querySelectorAll('.removeBtn');
+
+    for(let i = 0; i < removeBtn.length; i++) {
+        removeBtn[i].onclick = function() {
+            const parentDiv = this.parentElement;
+            const parentLi = parentDiv.parentElement;
+            parentLi.style.display = 'none';
+        }
     }
 }
 
 
-// const activeTask = document.querySelectorAll('.taskDesc');
-// console.log(activeTask);
+// Cross out task
+function crossOutP() {    
+    const taskDesc = document.querySelectorAll('.taskDesc');
 
-// for(let i = 0; i < activeTask.length; i++) {
-//     activeTask[i].onclick = function() {
-//         const parentDiv = this.parentElement;
-//         parentDiv.style.textDecoration = 'line-through';
-//     }
-// }
-
-
-const checkbox = document.querySelectorAll('.checkbox');
-console.log(checkbox);
-
-const taskDesc = document.querySelectorAll('.taskDesc');
-console.log(taskDesc);
-
-for(let i = 0; i < checkbox.length; i++) {
-    checkbox[i].onclick = function() {
-        taskDesc.style.textDecoration = 'line-through';
+    for(let i = 0; i < taskDesc.length; i++) {
+        taskDesc[i].onclick = function() {
+            this.classList.toggle('crossOut');
+        }
     }
 }
